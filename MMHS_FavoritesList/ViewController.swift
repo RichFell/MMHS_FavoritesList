4efa56d745f8209e42f3f0a00c68692d3ac1b874
Rich Fellure <rfellure24@gmail.com>
2014-07-14 15:27:14 -0500

    Finished building MVP for app

--- MMHS_FavoritesList/ViewController.swift
+++ MMHS_FavoritesList/ViewController.swift
@@ -8,18 +8,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
                             
-    override func viewDidLoad() {
+    @IBOutlet var tableView: UITableView
+    var favorites = [String]()
+
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        favorites = ["Snowboarding", "Coding", "Muay Thai", "Jet Skiing"]
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func onButtonPressedAddFavorite(sender: AnyObject)
+    {
+        let alert = UIAlertController(title: "Add another favorite activity", message: nil, preferredStyle: .Alert)
+        alert.addTextFieldWithConfigurationHandler({ textField in
+            textField.placeholder = "Add a new favorite"
+            })
+
+        let cancelAction = UIAlertAction(title: "Cancel", style:.Cancel, handler: ({ action in
+
+            }))
+        alert.addAction(cancelAction)
+
+        let saveAction = UIAlertAction(title: "Add", style: .Default, handler: ({ action in
+
+            let textField = alert.textFields[0] as UITextField
+            self.favorites.append(textField.text)
+
+            self.tableView.reloadData()
+            }))
+        alert.addAction(saveAction)
+
+        self.presentViewController(alert, animated: true, completion: nil)
+
     }
 
+    func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell!
+    {
+        let cell = tableView.dequeueReusableCellWithIdentifier("CellID", forIndexPath: indexPath) as UITableViewCell
+        let favorite = favorites[indexPath.row] as String
 
+        cell.textLabel.text = favorite
+
+        return cell
+    }
+
+    func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int
+    {
+        return favorites.count
+    }
 }
 
